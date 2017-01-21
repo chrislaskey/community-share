b9661d67b96fbf7c21996faba1c7e94ab7f24ef1
Chris Laskey <claskey@bluebox.net>
2017-01-20 23:11:41 -0500

    Add Documents model

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,29 @@
+class DocumentsController < ApplicationController
+
+  def index
+    @documents = Document.all
+  end
+
+  def show
+    @document = Document.find(params[:id])
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(permitted_params)
+
+    @document.save
+
+    redirect_to @document
+  end
+
+  private
+
+  def permitted_params
+    params.require(:document).permit(:name)
+  end
+
+end
---
+++ db/migrate/20170121035418_create_documents.rb
@@ -0,0 +1,8 @@
+class CreateDocuments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :documents do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
