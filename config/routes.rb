d0004e93df993eae89342e03fbc9048d55b3ebfa
Chris Laskey <claskey@bluebox.net>
2017-01-29 18:10:17 -0500

    Add session destroy endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :user_log_out
+  end
+
   resources :documents
   resources :document_tags, path: "document-tags", only: [:show]
   resources :tags
