9d599b203cb734e5374531c7d3e3caed4cef5665
Chris Laskey <claskey@bluebox.net>
2017-02-01 20:16:33 -0500

    Add user new and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all.order(:role, :name)
@@ -9,6 +9,22 @@ class UsersController < ApplicationController
 
   def show
 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(permitted_params)
+
+    if @user.save
+      flash[:success] = ["Successfully created #{@user.name}"]
+      redirect_to users_path
+    else
+      flash[:error] = @user.errors.full_messages
+      render "new"
+    end
   end
 
   def edit
@@ -23,11 +39,23 @@ class UsersController < ApplicationController
 
     if @user.update_attributes(permitted_params)
       flash[:success] = ["Successfully updated #{@user.name}"]
-      redirect_to @user
+      redirect_to users_path
     else
       flash[:error] = @user.errors.full_messages
       render "edit"
     end
+  end
+
+  def destroy
+    if is_current_user?
+      flash[:error] = ["Can not remove own user"]
+      return redirect_to users_path
+    end
+
+    @user.destroy
+
+    flash[:success] = ["Successfully deleted #{@user.name}"]
+    redirect_to users_path
   end
 
   private
@@ -37,11 +65,15 @@ class UsersController < ApplicationController
   end
 
   def permitted_params
-    params.require(:user).permit(:role)
+    params.require(:user).permit(:email, :name, :role)
+  end
+
+  def is_current_user?
+    current_user.id.to_s == params[:id]
   end
 
   def updating_own_role?
-    current_user.id.to_s == params[:id] && current_user.role != params[:user][:role]
+    is_current_user? && current_user.role != params[:user][:role]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ApplicationRecord
 
+  include ActionView::Helpers::DateHelper
+
   ROLES = %w(admin default pending)
 
   devise :rememberable, :timeoutable, :trackable, :omniauthable, omniauth_providers: [:facebook]
 
-  validates :role, inclusion: {
-    in: User::ROLES
-  }
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :role, inclusion: { in: User::ROLES }
 
   after_initialize :set_role
+
+  def last_sign_in_ago
+    last_sign_in_at ? time_ago_in_words(last_sign_in_at) + "ago" : "-"
+  end
 
   private
 
