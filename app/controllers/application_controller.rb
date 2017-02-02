0e0a9bb9569de04726630c1db538d5f8a690b07c
Chris Laskey <claskey@bluebox.net>
2017-02-01 20:41:25 -0500

    Ensure user is authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   helper_method :current_user
 
