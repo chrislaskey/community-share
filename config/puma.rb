27c0368d086a5a81f77c0f6e062623ee31e31025
Chris Laskey <claskey@bluebox.net>
2017-02-02 22:19:24 -0500

    Update puma config to init.d format

--- config/puma.rb
+++ config/puma.rb
@@ -7,11 +7,27 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
+# Directories
+#
+app_dir = File.expand_path("../..", __FILE__)
+temp_dir = "#{app_dir}/tmp/puma"
+
+# Rails ENV
+#
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
+
+# Socket
+#
+bind "unix://#{temp_dir}/puma/socket"
+
+# Logging
+#
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -23,7 +39,7 @@ environment rails_env
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -41,31 +57,24 @@ environment rails_env
 # or connections that may have been created at application boot, Ruby
 # cannot share connections between processes.
 #
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
-
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
-
-workers 1
-
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
-
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
+
+# PID
+#
+pidfile "#{temp_dir}/pid"
+
+# State Path
+#
+state_path "#{temp_dir}/state"
+
+# Activate
+#
+activate_control_app
+
+# Allow puma to be restarted by `rails restart` command.
+#
+plugin :tmp_restart
