fd674f76f548f275c966102027ed72537a795d09
Chris Laskey <contact@chrislaskey.com>
2017-02-18 17:01:39 -0500

    Round out community subscription specs

--- spec/factories/community_subscription.rb
+++ spec/factories/community_subscription.rb
@@ -10,4 +10,3 @@ FactoryGirl.define do
   end
 
 end
-
--- spec/models/community_subscription_spec.rb
+++ spec/models/community_subscription_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe CommunitySubscription, type: :model do
 
-  let(:community_subscription) { create(:community_subscription) }
+  let(:subscription) { create(:community_subscription) }
 
   describe "associations" do
     it { is_expected.to belong_to(:community) }
@@ -11,13 +11,53 @@ RSpec.describe CommunitySubscription, type: :model do
 
   describe "aliases" do
     it "has a level alias" do
-      expect(community_subscription).to respond_to(:level)
+      expect(subscription).to respond_to(:level)
     end
   end
 
   describe "validations" do
     it { is_expected.to validate_presence_of(:community) }
     it { is_expected.to validate_presence_of(:community_level) }
+    it { is_expected.to validate_presence_of(:started_at) }
+  end
+
+  describe "before actions" do
+    describe "close_active_subscription" do
+      let(:community) { create(:community) }
+      let(:open_subscription) { create(:community_subscription, community: community) }
+      let(:new_subscription) { build(:community_subscription, community: community) }
+
+      let(:other_community) { create(:community) }
+      let(:other_open_subscription) { create(:community_subscription, community: other_community) }
+
+      it "closes existing communities subscriptions on close" do
+        expect(open_subscription.active).to be_truthy
+
+        new_subscription.save
+
+        expect(open_subscription.reload.active).to be_falsey
+      end
+
+      it "does not affect other community subscriptions" do
+        expect(other_open_subscription.active).to be_truthy
+
+        new_subscription.save
+
+        expect(other_open_subscription.reload.active).to be_truthy
+      end
+    end
+  end
+
+  describe "active" do
+    it "is true when ended_at is nil" do
+      subscription.ended_at = nil
+      expect(subscription.active).to be_truthy
+    end
+
+    it "is false when ended_at is present" do
+      subscription.ended_at = Time.now
+      expect(subscription.active).to be_falsey
+    end
   end
 
 end
